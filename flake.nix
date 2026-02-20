572f602a818f44b24251487b9b360ea032ed3a72
feline-dis <kriah0872@gmail.com>
2026-02-19 17:07:25 -0700

    Add Nix flake for registration-proxy package
    Exposes the Go registration proxy as a Nix package via buildGoModule.
    Relaxes go.mod version constraint to work with nixpkgs Go toolchain.

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Matrix registration proxy for ohana-matrix.xyz";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          registration-proxy = pkgs.buildGoModule {
+            pname = "registration-proxy";
+            version = "0.1.0";
+            src = ./registration;
+            vendorHash = null;
+            postInstall = ''
+              mv $out/bin/registration $out/bin/registration-proxy
+            '';
+          };
+
+          default = self.packages.${system}.registration-proxy;
+        });
+    };
+}
--- registration/go.mod
+++ registration/go.mod
@@ -1,3 +1,3 @@
 module feline-matrix/registration
 
-go 1.25.7
+go 1.25
